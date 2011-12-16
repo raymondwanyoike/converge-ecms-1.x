eb90a2addb9607ccb84dc48b104c2754b240c41c
Allan Lykke Christensen <allan@i2m.dk>
2011-12-16 16:02:32 +0300

    Added comment for fixing issue #488

--- converge-server/modules/converge-ws/src/main/java/dk/i2m/converge/ws/soap/OutletService.java
+++ converge-server/modules/converge-ws/src/main/java/dk/i2m/converge/ws/soap/OutletService.java
@@ -1,11 +1,18 @@
 /*
  * Copyright (C) 2011 Interactive Media Management
  *
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
  *
- * You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package dk.i2m.converge.ws.soap;
 
@@ -35,15 +42,18 @@ import javax.jws.WebService;
 @WebService(serviceName = "OutletService")
 public class OutletService {
 
-    private static final Logger LOG = Logger.getLogger(OutletService.class.getName());
+    private static final Logger LOG = Logger.getLogger(OutletService.class.
+            getName());
 
     @EJB private OutletFacadeLocal outletFacade;
 
     @WebMethod(operationName = "getEditions")
-    public List<dk.i2m.converge.ws.model.Edition> getEditions(@WebParam(name = "outletId") Long outletId, @WebParam(name = "date") Date date) {
+    public List<dk.i2m.converge.ws.model.Edition> getEditions(@WebParam(name =
+            "outletId") Long outletId, @WebParam(name = "date") Date date) {
         Calendar editionDate = Calendar.getInstance();
         editionDate.setTime(date);
-        List<dk.i2m.converge.core.workflow.Edition> editions = outletFacade.findEditionByOutletAndDate(outletId, editionDate);
+        List<dk.i2m.converge.core.workflow.Edition> editions = outletFacade.
+                findEditionByOutletAndDate(outletId, editionDate);
         List<Edition> outputEditions = new ArrayList<Edition>();
 
         for (dk.i2m.converge.core.workflow.Edition edition : editions) {
@@ -59,8 +69,14 @@ public class OutletService {
     }
 
     @WebMethod(operationName = "createEdition")
-    public Edition createEdition(@WebParam(name = "outletId") Long outletId, @WebParam(name = "open") boolean open, @WebParam(name = "publicationDate") Date publicationDate, @WebParam(name = "expirationDate") Date expirationDate, @WebParam(name = "closeDate") Date closeDate) {
-        dk.i2m.converge.core.workflow.Edition edition = outletFacade.createEdition(outletId, open, publicationDate, expirationDate, closeDate);
+    public Edition createEdition(@WebParam(name = "outletId") Long outletId,
+            @WebParam(name = "open") boolean open, @WebParam(name =
+            "publicationDate") Date publicationDate, @WebParam(name =
+            "expirationDate") Date expirationDate,
+            @WebParam(name = "closeDate") Date closeDate) {
+        dk.i2m.converge.core.workflow.Edition edition = outletFacade.
+                createEdition(outletId, open, publicationDate, expirationDate,
+                closeDate);
         Edition output = new Edition();
         output.setId(edition.getId());
         output.setCloseDate(edition.getCloseDate());
@@ -71,16 +87,16 @@ public class OutletService {
 
     /**
      * Obtains a given {@link Outlet} and its {@link Section}s.
-     * 
-     * @param id
-     *          Unique identifier of the {@link Outlet}
+     *
+     * @param id Unique identifier of the {@link Outlet}
      * @return {@link Outlet} matching the given {@code id}
      */
     @WebMethod(operationName = "getOutlet")
     public Outlet getOutlet(@WebParam(name = "outletId") Long id) {
         Outlet outlet = new Outlet();
         try {
-            dk.i2m.converge.core.workflow.Outlet convergeOutlet = outletFacade.findOutletById(id);
+            dk.i2m.converge.core.workflow.Outlet convergeOutlet;
+            convergeOutlet = outletFacade.findOutletById(id);
             outlet = ModelConverter.toOutlet(convergeOutlet);
         } catch (DataNotFoundException ex) {
             LOG.log(Level.WARNING, "Unknown outlet requested");
@@ -89,7 +105,11 @@ public class OutletService {
     }
 
     @WebMethod(operationName = "createSection")
-    public Long createSection(@WebParam(name = "outletId") Long outletId, @WebParam(name = "parentId") Long parentSectionId, @WebParam(name = "name") String name, @WebParam(name = "description") String description, @WebParam(name = "active") boolean active) throws DataNotFoundException {
+    public Long createSection(@WebParam(name = "outletId") Long outletId,
+            @WebParam(name = "parentId") Long parentSectionId, @WebParam(name =
+            "name") String name,
+            @WebParam(name = "description") String description, @WebParam(name =
+            "active") boolean active) throws DataNotFoundException {
         Section section = new Section();
         section.setActive(true);
         section.setDescription(description);
@@ -103,12 +123,18 @@ public class OutletService {
     }
 
     @WebMethod(operationName = "findSectionByName")
-    public List<dk.i2m.converge.ws.model.Section> findSectionByName(@WebParam(name = "outletId") Long outletId, @WebParam(name = "section") String sectionName) throws DataNotFoundException {
-        List<Section> sections = outletFacade.findSectionByName(outletId, sectionName);
-        List<dk.i2m.converge.ws.model.Section> results = new ArrayList<dk.i2m.converge.ws.model.Section>();
-        
+    public List<dk.i2m.converge.ws.model.Section> findSectionByName(@WebParam(name =
+            "outletId") Long outletId,
+            @WebParam(name = "section") String sectionName) throws
+            DataNotFoundException {
+        List<Section> sections = outletFacade.findSectionByName(outletId,
+                sectionName);
+        List<dk.i2m.converge.ws.model.Section> results =
+                new ArrayList<dk.i2m.converge.ws.model.Section>();
+
         for (Section section : sections) {
-            dk.i2m.converge.ws.model.Section result = new dk.i2m.converge.ws.model.Section();
+            dk.i2m.converge.ws.model.Section result =
+                    new dk.i2m.converge.ws.model.Section();
             result.setId(section.getId());
             result.setTitle(section.getName());
             results.add(result);
@@ -118,9 +144,8 @@ public class OutletService {
 
     /**
      * Obtains the published news items in a given edition.
-     * 
-     * @param id
-     *          Unique identifier of the {@link Edition}
+     *
+     * @param id Unique identifier of the {@link Edition}
      * @return {@link Edition} containing the published {@link NewsItem}s
      */
     @WebMethod(operationName = "getPublishedEdition")
@@ -128,12 +153,15 @@ public class OutletService {
         Edition edition = new Edition();
 
         try {
-            dk.i2m.converge.core.workflow.Edition convergeEdition = outletFacade.findEditionById(id);
+            dk.i2m.converge.core.workflow.Edition convergeEdition;
+            convergeEdition = outletFacade.findEditionById(id);
 
             edition.setId(convergeEdition.getId());
             edition.setCloseDate(convergeEdition.getCloseDate());
-            edition.setPublicationDate(convergeEdition.getPublicationDate().getTime());
-            edition.setExpirationDate(convergeEdition.getExpirationDate().getTime());
+            edition.setPublicationDate(convergeEdition.getPublicationDate().
+                    getTime());
+            edition.setExpirationDate(convergeEdition.getExpirationDate().
+                    getTime());
 
             for (NewsItemPlacement nip : convergeEdition.getPlacements()) {
                 if (nip.getNewsItem().isEndState()) {
