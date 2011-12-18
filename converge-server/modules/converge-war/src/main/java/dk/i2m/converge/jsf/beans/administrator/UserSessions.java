dea4d856760eabf0464fab8cd909c399f2a582e3
Allan Lykke Christensen <allan@i2m.dk>
2011-12-18 03:08:34 +0300

    Shows last activity using pretty time

--- converge-server/modules/converge-war/src/main/java/dk/i2m/converge/jsf/beans/administrator/UserSessions.java
+++ converge-server/modules/converge-war/src/main/java/dk/i2m/converge/jsf/beans/administrator/UserSessions.java
@@ -1,11 +1,18 @@
 /*
  * Copyright (C) 2010 - 2011 Interactive Media Management
  *
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
  *
- * You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package dk.i2m.converge.jsf.beans.administrator;
 
@@ -13,17 +20,18 @@ import dk.i2m.converge.core.security.UserAccount;
 import dk.i2m.converge.ejb.facades.UserFacadeLocal;
 import dk.i2m.converge.ejb.services.DataNotFoundException;
 import dk.i2m.converge.web.ActiveUserSession;
+import dk.i2m.jsf.JsfUtils;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.model.DataModel;
 import javax.faces.model.ListDataModel;
 import javax.servlet.http.HttpSession;
 
 /**
- * Backing bean for
- * <code>/administrator/UserSessions.jspx</code>.
+ * Backing bean for {@code /administrator/UserSessions.jspx}.
  *
  * @author Allan Lykke Christensen
  */
@@ -42,10 +50,12 @@ public class UserSessions {
     }
 
     public DataModel getSessions() {
-        List<ActiveUserSession> activeSessions = new ArrayList<ActiveUserSession>();
+        List<ActiveUserSession> activeSessions =
+                new ArrayList<ActiveUserSession>();
         anonymousSessions = 0;
 
-        List<HttpSession> sessionRegistry = dk.i2m.converge.web.UserSessions.getInstance().getCurrentSessions();
+        List<HttpSession> sessionRegistry = dk.i2m.converge.web.UserSessions.
+                getInstance().getCurrentSessions();
 
         for (HttpSession httpSession : sessionRegistry) {
 
@@ -85,22 +95,32 @@ public class UserSessions {
         return this.sessions;
     }
 
+    /**
+     * Get the number of anonymous sessions.
+     * 
+     * @return Number of anonymous user sessions
+     */
     public int getAnonymousSessions() {
         return anonymousSessions;
     }
 
     /**
      * Invalidates an {@link ActiveUserSession}.
-     * 
-     * @param session
-     *          {@link ActiveUserSession} to invalidate
+     *
+     * @param session {@link ActiveUserSession} to invalidate
      */
     public void setInvalidateSession(ActiveUserSession session) {
-        List<HttpSession> sessionRegistry = dk.i2m.converge.web.UserSessions.getInstance().getCurrentSessions();
+        List<HttpSession> sessionRegistry = dk.i2m.converge.web.UserSessions.
+                getInstance().getCurrentSessions();
 
         for (HttpSession httpSession : sessionRegistry) {
             if (httpSession.getId().equals(session.getSessionId())) {
                 httpSession.invalidate();
+                JsfUtils.createMessage("frmUserSessions",
+                        FacesMessage.SEVERITY_INFO, "i18n",
+                        "administrator_UserSessions_USER_SESSION_TERMINATED",
+                        null);
+                return;
             }
         }
     }
