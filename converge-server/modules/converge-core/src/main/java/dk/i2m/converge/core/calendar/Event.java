20829d7d716147d7f24ca949df0fa52ffa65545c
Allan Lykke Christensen <allan@i2m.dk>
2012-02-12 16:01:44 +0300

    Cleaned up imports

--- converge-server/modules/converge-core/src/main/java/dk/i2m/converge/core/calendar/Event.java
+++ converge-server/modules/converge-core/src/main/java/dk/i2m/converge/core/calendar/Event.java
@@ -22,21 +22,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
+import javax.persistence.*;
 
 /**
  * Domain model object representing an event in a calendar.
