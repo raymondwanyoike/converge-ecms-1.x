4c36ec3414e4568a2ea1549676477fa8750bbc5a
Allan Lykke Christensen <allan@i2m.dk>
2012-05-05 00:47:10 +0200

    Channel that will contain the configuration of where the MediaItems will be distributed

---
+++ converge-server/modules/converge-core/src/main/java/dk/i2m/converge/core/content/catalogue/Channel.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2012 Interactive Media Management
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dk.i2m.converge.core.content.catalogue;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Channel for distributing {@link MediaItem}s. The purpose of a channel is to
+ * distribute (upload) {@link MediaItem}s separately from {@link NewsItem}s.
+ *
+ * @author Allan Lykke Christensen
+ */
+@Entity
+@Table(name = "channel")
+public class Channel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Channel)) {
+            return false;
+        }
+        Channel other = (Channel) object;
+        if ((this.id == null && other.id != null) || (this.id != null
+                && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "dk.i2m.converge.core.content.catalogue.Channel[ id=" + id + " ]";
+    }
+}
