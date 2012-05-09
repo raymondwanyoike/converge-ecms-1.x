fd7873a9f132499a97afbea0eef5e325830e998f
Allan Lykke Christensen <allan@i2m.dk>
2012-05-09 21:53:36 +0200

    Initial fields for the Channel entity

--- converge-server/modules/converge-core/src/main/java/dk/i2m/converge/core/content/catalogue/Channel.java
+++ converge-server/modules/converge-core/src/main/java/dk/i2m/converge/core/content/catalogue/Channel.java
@@ -34,18 +34,106 @@ public class Channel implements Serializable {
     @Column(name = "id")
     private Long id;
 
+    @Column(name = "enabled")
+    private boolean enabled = true;
+
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "description")
+    @Lob
     private String description;
 
+    /**
+     * Creates a new instance of {@link Channel}.
+     */
+    public Channel() {
+    }
+
+    /**
+     * Gets the unique identifier of the {@link Channel} assigned by the
+     * underlying relational database.
+     * 
+     * @return Unique identifier of the {@link Channel}
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets the unique identifier of the {@link Channel}.
+     * 
+     * @param id 
+     *          Unique identifier of the {@link Channel}
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Gets the name of the {@link Channel}. The name is used for identification
+     * by the user.
+     * 
+     * @return Name of the {@link Channel}
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the name of the {@link Channel}.
+     * 
+     * @param name 
+     *          Name of the {@link Channel}
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Sets a description of the {@link Channel}. The description is used for
+     * describing the purpose of the {@link Channel}.
+     * 
+     * @return Description of the {@link Channel}
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets the description of the {@link Channel}.
+     * 
+     * @param description 
+     *          Description of the {@link Channel}
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Determine if the {@link Channel} is enabled. An enabled channel is
+     * visible and usable to the users, whereas a disabled channel is not 
+     * visible and can not be accessed.
+     * 
+     * @return {@code true} if the {@link Channel} is enabled, otherwise
+     *         {@code false} 
+     */
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    /**
+     * Sets the status of the {@link Channel}.
+     * 
+     * @param enabled
+     *          {@code true} if the {@link Channel} is enabled, otherwise
+     *          {@code false}
+     */
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    /** {@inheritDoc } */
     @Override
     public int hashCode() {
         int hash = 0;
@@ -53,6 +141,15 @@ public class Channel implements Serializable {
         return hash;
     }
 
+    /**
+     * {@link Channel}s with the same unique identifier are considered to be
+     * equal, all others are not.
+     * 
+     * @param object 
+     *          {@link Channel} to compare
+     * @return {@code true} if the {@code object} has the same unique
+     *         identifier, otherwise {@code false}
+     **/
     @Override
     public boolean equals(Object object) {
         if (!(object instanceof Channel)) {
@@ -66,8 +163,9 @@ public class Channel implements Serializable {
         return true;
     }
 
+    /** {@inheritDoc } */
     @Override
     public String toString() {
-        return "dk.i2m.converge.core.content.catalogue.Channel[ id=" + id + " ]";
+        return getClass().getName() + "[id=" + this.id + "]";
     }
 }
