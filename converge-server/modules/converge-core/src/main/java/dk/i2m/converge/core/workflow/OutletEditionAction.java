464f20c63f5d4e0421a5268f7f58d48d9db60f36
Allan Lykke Christensen <allan@i2m.dk>
2012-05-09 21:45:49 +0200

    Optimised imports

--- converge-server/modules/converge-core/src/main/java/dk/i2m/converge/core/workflow/OutletEditionAction.java
+++ converge-server/modules/converge-core/src/main/java/dk/i2m/converge/core/workflow/OutletEditionAction.java
@@ -22,17 +22,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import org.eclipse.persistence.annotations.PrivateOwned;
 
 /**
