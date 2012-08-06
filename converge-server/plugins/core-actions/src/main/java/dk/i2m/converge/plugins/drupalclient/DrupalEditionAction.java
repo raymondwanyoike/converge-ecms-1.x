bf948f3b0df00015d4359264409bdaaeec5efc20
Raymond Wanyoike <rwa@i2m.dk>
2012-08-06 03:15:20 +0300

    Implement services-api-client

--- converge-server/plugins/core-actions/src/main/java/dk/i2m/converge/plugins/drupalclient/DrupalEditionAction.java
+++ converge-server/plugins/core-actions/src/main/java/dk/i2m/converge/plugins/drupalclient/DrupalEditionAction.java
@@ -17,17 +17,44 @@
 package dk.i2m.converge.plugins.drupalclient;
 
 import dk.i2m.converge.core.annotations.OutletAction;
+import dk.i2m.converge.core.content.NewsItem;
+import dk.i2m.converge.core.content.NewsItemActor;
+import dk.i2m.converge.core.content.NewsItemEditionState;
+import dk.i2m.converge.core.content.NewsItemMediaAttachment;
 import dk.i2m.converge.core.content.NewsItemPlacement;
+import dk.i2m.converge.core.content.catalogue.MediaItem;
+import dk.i2m.converge.core.content.catalogue.MediaItemRendition;
+import dk.i2m.converge.core.content.catalogue.RenditionNotFoundException;
 import dk.i2m.converge.core.plugin.EditionAction;
 import dk.i2m.converge.core.plugin.PluginContext;
 import dk.i2m.converge.core.workflow.Edition;
 import dk.i2m.converge.core.workflow.OutletEditionAction;
+import dk.i2m.converge.core.workflow.Section;
+import dk.i2m.drupal.DefaultDrupalClient;
+import dk.i2m.drupal.core.DrupalClient;
+import dk.i2m.drupal.fields.Image;
+import dk.i2m.drupal.fields.wrappers.BasicWrapper;
+import dk.i2m.drupal.fields.wrappers.ImageWrapper;
+import dk.i2m.drupal.fields.wrappers.ListWrapper;
+import dk.i2m.drupal.fields.wrappers.TextWrapper;
+import dk.i2m.drupal.resources.FileResource;
+import dk.i2m.drupal.resources.NodeResource;
+import dk.i2m.drupal.resources.UserResource;
+import dk.i2m.drupal.response.FileResponce;
+import dk.i2m.drupal.response.NodeResponse;
+import dk.i2m.drupal.util.HttpMessageBuilder;
+import java.io.File;
+import java.net.URI;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -161,7 +188,155 @@ public class DrupalEditionAction implements EditionAction {
                     "'socketTimeout' must be an integer");
         }
 
+        LOG.log(Level.INFO, "Starting action... Edition #{0}", edition.getId());
+
+        setSectionMapping(mappings);
+
+        DrupalClient dc =
+                new DefaultDrupalClient(URI.create(hostname), endpoint, Integer.
+                parseInt(connectionTimeout), Integer.parseInt(socketTimeout));
+        UserResource ur = new UserResource(dc, username, password);
+        FileResource fr = new FileResource(dc);
+        NodeResource nr = new NodeResource(dc);
+
+        int errors = 0;
+
+        try {
+            dc.setup();
+            ur.login();
+
+            LOG.log(Level.INFO, "Found {0} NewsItem(s)", edition.
+                    getNumberOfPlacements());
+
+            for (NewsItemPlacement nip : edition.getPlacements()) {
+                HttpMessageBuilder fb = new HttpMessageBuilder();
+
+                if (nodeLanguage != null) {
+                    fb = new HttpMessageBuilder(nodeLanguage);
+                }
+
+                NewsItem newsItem = nip.getNewsItem();
+
+                fb
+                        .add(new BasicWrapper("title", getTitle(newsItem)))
+                        .add(new BasicWrapper("type", nodeType))
+                        .add(new BasicWrapper("status", getStatus()))
+                        .add(new BasicWrapper("promote", getPromoted(nip)))
+                        .add(new BasicWrapper("publish_on", getPublishOn()))
+                        .add(new TextWrapper("body", newsItem.getBrief(),
+                        newsItem.getStory(), "full_html"))
+                        .add(
+                        new TextWrapper("field_author", getAuthor(newsItem)))
+                        .add(new TextWrapper("field_converge", newsItem.getId().
+                        toString()));
+
+                if (!getSection(nip).isEmpty()) {
+                    fb.add(new ListWrapper("field_section", getSection(nip)));
+                }
+
+                List<MediaItem> mediaItems = getMediaItems(newsItem);
+
+                try {
+                    ImageWrapper imageWrapper = new ImageWrapper("field_image");
+                    
+                    for (MediaItem mediaItem : mediaItems) {
+                        MediaItemRendition mir = mediaItem.getOriginal();
+
+                        try {
+                            mir = mediaItem.findRendition(renditionName);
+                        } catch (RenditionNotFoundException ex) {
+                            LOG.log(Level.SEVERE,
+                                    "Rendition ''{0}'' missing for MediaItem #{1} - NewsItem #{2}",
+                                    new Object[]{renditionName, mediaItem.
+                                        getId(), newsItem.getId()});
+                        }
+
+                        File file = new File(mir.getFileLocation());
+                        String title = mediaItem.getTitle();
+
+                        FileResponce fileResponce = fr.createRaw(file, title);
+
+                        String fid = fileResponce.getId().toString();
+                        String alt = truncateString(mediaItem.getDescription(), 512);
+                        String description = truncateString(mediaItem.
+                                getDescription(), 1024);
+
+                        imageWrapper.add(new Image(fid, alt, description));
+                    }
+                    
+                    if (!mediaItems.isEmpty()) {
+                        fb.add(imageWrapper);
+                    }
+                } catch (Exception ex) {
+                    LOG.log(Level.SEVERE,
+                            "> Uploading NewsItem #{0} image(s) failed",
+                            newsItem.getId());
+                    LOG.log(Level.SEVERE, null, ex);
+
+                    errors++;
+
+                    if (errors > 5) {
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+
+                NewsItemEditionState status = ctx.
+                        addNewsItemEditionState(edition.getId(), newsItem.
+                        getId(), STATUS_LABEL, UPLOADING.toString());
+                NewsItemEditionState nid = ctx.addNewsItemEditionState(edition.
+                        getId(), newsItem.getId(), NID_LABEL, null);
+                NewsItemEditionState uri = ctx.addNewsItemEditionState(edition.
+                        getId(), newsItem.getId(), URI_LABEL, null);
+                NewsItemEditionState submitted = ctx.
+                        addNewsItemEditionState(edition.getId(), newsItem.
+                        getId(), SUBMITTED, null);
+
+                LOG.log(Level.INFO, "> Uploading NewsItem #{0} & {1} image(s)",
+                        new Object[]{newsItem.getId(), mediaItems.size()});
+
+                try {
+                    NodeResponse nodeResponse = nr.create(fb.
+                            toUrlEncodedFormEntity());
+
+                    nid.setValue(nodeResponse.getId().toString());
+                    uri.setValue(nodeResponse.getUri().toString());
+                    submitted.setValue(new Date().toString());
+                    status.setValue(UPLOADED.toString());
+                } catch (Exception ex) {
+                    status.setValue(FAILED.toString());
+                    LOG.log(Level.SEVERE, null, ex);
+
+                    ctx.updateNewsItemEditionState(status);
+                    ctx.updateNewsItemEditionState(nid);
+                    ctx.updateNewsItemEditionState(uri);
+                    ctx.updateNewsItemEditionState(submitted);
+
+                    errors++;
+
+                    if (errors > 5) {
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+
+                ctx.updateNewsItemEditionState(status);
+                ctx.updateNewsItemEditionState(nid);
+                ctx.updateNewsItemEditionState(uri);
+                ctx.updateNewsItemEditionState(submitted);
+            }
+
+            LOG.log(Level.INFO, "Encountered {0} error(s)", errors);
+            
+            ur.logout();
+            dc.shutdown();
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, null, ex);
         }
+
+        LOG.log(Level.INFO, "Finishing action... Edition #{0}", edition.getId());
     }
 
     @Override
@@ -226,6 +401,177 @@ public class DrupalEditionAction implements EditionAction {
     @Override
     public String getAbout() {
         return bundle.getString("PLUGIN_ABOUT");
+    }
+
+    private void setSectionMapping(String mapping) {
+        String[] values = mapping.split(";");
+
+        LOG.log(Level.INFO, "Found {0} Section mapping(s)", values.length);
+
+        for (int i = 0; i < values.length; i++) {
+            String[] value = values[i].split(":");
+
+            Long convergeId = Long.valueOf(value[0].trim());
+            Long drupalId = Long.valueOf(value[1].trim());
+            sectionMapping.put(convergeId, drupalId);
+        }
+    }
+
+    /**
+     * Truncate text.
+     * 
+     * @param value text
+     * @param length length to truncate
+     * @return truncated text
+     */
+    private String truncateString(String value, int length) {
+        if (value != null && value.length() > length) {
+            value = value.trim().substring(0, length - 3) + "...";
+        }
+
+        return value;
+    }
+
+    /**
+     * Get Title text value.
+     * 
+     * @param newsItem {@link NewsItem}
+     * @return
+     */
+    private String getTitle(NewsItem newsItem) {
+        // Must be < 255 in length
+        return truncateString(newsItem.getTitle(), 255);
+    }
+
+    /**
+     * Get Published checkbox value.
+     * 
+     * @return
+     */
+    private String getStatus() {
+        if (publishImmediately != null) {
+            return "1";
+        } else {
+            // see getPublishOn()
+            return "0";
+        }
+    }
+
+    /**
+     * Get Publish on text value.
+     * 
+     * @return "YYYY-MM-DD HH:MM:SS" or ""
+     */
+    private String getPublishOn() {
+        if (publishImmediately != null) {
+            return "";
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.HOUR_OF_DAY, Integer.valueOf(publishDelay));
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return sdf.format(calendar.getTime());
+    }
+
+    /**
+     * Get Promoted to front page checkbox value.
+     * 
+     * @param placement {@link NewsItemPlacement}
+     * @return
+     */
+    private String getPromoted(NewsItemPlacement placement) {
+        if (placement.getStart() == 1) {
+            return "1";
+        } else {
+            return "0";
+        }
+    }
+
+    /**
+     * Get Author text field.
+     * 
+     * @param newsItem {@link NewsItem}
+     * @return
+     */
+    private String getAuthor(NewsItem newsItem) {
+        if (newsItem.isUndisclosedAuthor()) {
+            return undisclosedAuthor;
+        } else {
+            if (newsItem.getByLine().trim().isEmpty()) {
+                StringBuilder sb = new StringBuilder();
+
+                for (NewsItemActor actor : newsItem.getActors()) {
+                    boolean firstActor = true;
+
+                    // TODO: Document this
+                    if (actor.getRole().equals(newsItem.getOutlet().
+                            getWorkflow().getStartState().getActorRole())) {
+                        if (!firstActor) {
+                            sb.append(", ");
+                        } else {
+                            firstActor = false;
+                        }
+
+                        sb.append(actor.getUser().getFullName());
+                    }
+                }
+
+                return sb.toString();
+            } else {
+                // Return the "by-line" of the NewsItem
+                return newsItem.getByLine();
+            }
+        }
+    }
+
+    /**
+     * Get Section taxonomy field.
+     * 
+     * @param nip {@link NewsItemPlacement}
+     * @return 
+     */
+    private String getSection(NewsItemPlacement nip) {
+        Section section = nip.getSection();
+
+        if (section != null) {
+            if (sectionMapping.containsKey(section.getId())) {
+                return section.getId().toString();
+            }
+        } else {
+            // Use fallback section
+            if (sectionMapping.containsKey(Long.parseLong("0"))) {
+                return sectionMapping.get(Long.parseLong("0")).toString();
+            }
+        }
+
+        LOG.log(Level.WARNING, "Section mapping failed for NewsItem #{0}",
+                nip.getNewsItem().getId());
+
+        return "";
+    }
+
+    /**
+     * Get {@link ImageField}s for {@link NewsItem}.
+     * 
+     * @param newsItem NewsItem
+     * @return 
+     */
+    private List<MediaItem> getMediaItems(NewsItem newsItem) {
+        List<MediaItem> mediaItems = new ArrayList<MediaItem>();
+
+        for (NewsItemMediaAttachment nima : newsItem.getMediaAttachments()) {
+            MediaItem mediaItem = nima.getMediaItem();
+
+            // Verify that the item exist and any renditions are attached
+            if (mediaItem == null || !mediaItem.isRenditionsAttached()) {
+                continue;
+            }
+
+            mediaItems.add(mediaItem);
+        }
+
+        return mediaItems;
     }
 
     private boolean isInteger(String input) {
